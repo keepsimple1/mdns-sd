c7b8cd34f6d8810c6a0439fdf2e46fdb8996a1f3
Luca Barbato <luca.barbato@gmail.com>
2021-12-29 20:12:37 +0100

    Add an example (#5)

---
+++ examples/query.rs
@@ -0,0 +1,33 @@
+//! A mDNS query client.
+//!
+//! Run with:
+//!
+//!     cargo run --example query <service_type>
+//!
+//! Keeps listening for new events.
+
+use mdns_sd::{ServiceDaemon, ServiceEvent};
+
+fn main() {
+    // Create a daemon
+    let mdns = ServiceDaemon::new().expect("Failed to create daemon");
+
+    let mut service_type = std::env::args()
+        .nth(1)
+        .expect("it requires a service_type as argument");
+
+    // Browse for a service type.
+    service_type.push_str(".local.");
+    let receiver = mdns.browse(&service_type).expect("Failed to browse");
+
+    while let Ok(event) = receiver.recv() {
+        match event {
+            ServiceEvent::ServiceResolved(info) => {
+                println!("Resolved a new service: {}", info.get_fullname());
+            }
+            other_event => {
+                println!("Received other event: {:?}", &other_event);
+            }
+        }
+    }
+}
