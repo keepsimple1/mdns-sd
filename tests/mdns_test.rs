6ed9140162c8c6717a88101f416da80c2735167b
Han Xu <keepsimple@gmail.com>
2022-02-05 11:33:58 -0800

    add logging to debug GitHub test error

--- tests/mdns_test.rs
+++ tests/mdns_test.rs
@@ -10,6 +10,8 @@ use std::{
 /// register(announce), browse(query), response, unregister, shutdown.
 #[test]
 fn integration_success() {
+    env_logger::init();
+
     // Create a daemon
     let d = ServiceDaemon::new().expect("Failed to create daemon");
 
