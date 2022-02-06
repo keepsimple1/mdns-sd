146d42a54931064c9df3ae8ec7fbc9ffa66b94ed
keepsimple1 <keepsimple1@users.noreply.github.com>
2022-02-05 22:18:20 -0800

    support special meta-query of dns-sd (#14)
    support `_services._dns-sd._udp.local.`

---
+++ examples/register.rs
@@ -0,0 +1,46 @@
+//! Registers a mDNS service.
+//!
+//! Run with:
+//!
+//! cargo run --example register <service_type> <instance_name>
+//!
+//! Example:
+//!
+//! cargo run --example register _my-hello._udp.local. test1
+//!
+
+use mdns_sd::{ServiceDaemon, ServiceInfo};
+
+fn main() {
+    // Create a new mDNS daemon.
+    let mdns = ServiceDaemon::new().expect("Could not create service daemon");
+    let service_type = std::env::args()
+        .nth(1)
+        .expect("require a service_type as the 1st argument");
+    let instance_name = std::env::args()
+        .nth(2)
+        .expect("require a instance_name as the 2nd argument");
+    let service_host_ipv4 = "192.168.1.111";
+    let service_hostname = "192.168.1.111.local.";
+    let port = 3456;
+
+    // Register a service.
+    let service_info = ServiceInfo::new(
+        &service_type,
+        &instance_name,
+        service_hostname,
+        service_host_ipv4,
+        port,
+        None,
+    );
+
+    mdns.register(service_info)
+        .expect("Failed to register mDNS service");
+
+    println!("Registered service {}.{}", &instance_name, &service_type);
+
+    // Running until ctrl-c or killed.
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(100))
+    }
+}
