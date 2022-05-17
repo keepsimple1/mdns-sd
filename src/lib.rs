d9b0d944d6fb94b8c4875342204655d34ceb980d
Han Xu <keepsimple@gmail.com>
2022-05-17 10:24:01 -0700

    impl AsIpv4Addrs refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1414,7 +1414,7 @@ impl AsIpv4Addrs for &str {
     }
 }
 
-impl AsIpv4Addrs for String {
+impl AsIpv4Addrs for &String {
     fn as_ipv4_addrs(&self) -> Result<HashSet<Ipv4Addr>> {
         self.as_str().as_ipv4_addrs()
     }
@@ -1440,16 +1440,16 @@ impl AsIpv4Addrs for () {
     }
 }
 
-impl AsIpv4Addrs for Ipv4Addr {
+impl AsIpv4Addrs for &Ipv4Addr {
     fn as_ipv4_addrs(&self) -> Result<HashSet<Ipv4Addr>> {
         let mut ips = HashSet::new();
-        ips.insert(*self);
+        ips.insert(**self);
 
         Ok(ips)
     }
 }
 
-impl AsIpv4Addrs for std::net::Ipv4Addr {
+impl AsIpv4Addrs for &std::net::Ipv4Addr {
     fn as_ipv4_addrs(&self) -> Result<HashSet<Ipv4Addr>> {
         let mut ips = HashSet::new();
         ips.insert(Ipv4Addr::from_std(self));
--- tests/addr_parse.rs
+++ tests/addr_parse.rs
@@ -1,8 +1,6 @@
-use std::collections::HashSet;
-use std::iter::FromIterator;
-
 use mdns_sd::AsIpv4Addrs;
 use nix::sys::socket::Ipv4Addr;
+use std::collections::HashSet;
 
 #[test]
 fn test_addr_str() {
@@ -18,7 +16,7 @@ fn test_addr_str() {
 
     let addr = "127.0.0.1".to_string();
     assert_eq!(
-        addr.as_ipv4_addrs(),
+        (&addr).as_ipv4_addrs(),
         Ok({
             let mut set = HashSet::new();
             set.insert(Ipv4Addr::from_std(&std::net::Ipv4Addr::new(127, 0, 0, 1)));
@@ -79,7 +77,7 @@ fn test_addr_ip() {
     let ip = std::net::Ipv4Addr::new(127, 0, 0, 1);
 
     assert_eq!(
-        ip.as_ipv4_addrs(),
+        (&ip).as_ipv4_addrs(),
         Ok({
             let mut set = HashSet::new();
             set.insert(Ipv4Addr::from_std(&std::net::Ipv4Addr::new(127, 0, 0, 1)));
@@ -89,7 +87,7 @@ fn test_addr_ip() {
     );
 
     assert_eq!(
-        Ipv4Addr::from_std(&ip).as_ipv4_addrs(),
+        (&Ipv4Addr::from_std(&ip)).as_ipv4_addrs(),
         Ok({
             let mut set = HashSet::new();
             set.insert(Ipv4Addr::from_std(&std::net::Ipv4Addr::new(127, 0, 0, 1)));
