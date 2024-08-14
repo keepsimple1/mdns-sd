4825f370d98cbc74a28ef436b069bb852d325170
hrzlgnm <hrzlgnm@users.noreply.github.com>
2024-08-14 19:29:39 +0200

    Remove accidentally added test batch file

--- test.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-:loop
-echo Running cargo test...
-cargo test --release --test mdns_test
-echo.
-echo Press Ctrl+C to stop or wait for the next run...
-timeout /t 1 /nobreak >nul
-goto loop
