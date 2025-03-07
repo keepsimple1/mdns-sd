d7eb5536e6d3a176bfe9f2fce789e6fa6ce9016b
CosminPerRam <cosmin.p@live.com>
2025-03-07 16:04:35 +0200

    chore: run cargo fmt

--- examples/query.rs
+++ examples/query.rs
@@ -16,7 +16,9 @@
 use mdns_sd::{ServiceDaemon, ServiceEvent};
 
 fn main() {
-    test_log::env_logger::builder().format_timestamp_millis().init();
+    test_log::env_logger::builder()
+        .format_timestamp_millis()
+        .init();
 
     // Create a daemon
     let mdns = ServiceDaemon::new().expect("Failed to create daemon");
