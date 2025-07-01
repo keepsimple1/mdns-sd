1759cac264295cc39ed96f66812c6b252748aaca
Han Xu <keepsimple@gmail.com>
2025-06-30 22:12:28 -0700

    address review comments

--- src/dns_parser.rs
+++ src/dns_parser.rs
@@ -115,7 +115,14 @@ impl fmt::Display for HostIp {
             HostIp::V4(v4) => write!(f, "{}", v4.addr),
             HostIp::V6(v6) => {
                 if v6.scope_id.index != 0 {
-                    write!(f, "{}%{}", v6.addr, v6.scope_id.name)
+                    #[cfg(not(windows))]
+                    {
+                        write!(f, "{}%{}", v6.addr, v6.scope_id.name)
+                    }
+                    #[cfg(windows)]
+                    {
+                        write!(f, "{}%{}", v6.addr, v6.scope_id.index)
+                    }
                 } else {
                     write!(f, "{}", v6.addr)
                 }
