c920b703e192e3c311313627dc06e3f3991b45ba
Han Xu <keepsimple@gmail.com>
2026-08-08 21:52:57 -0700

    fix inter-crate link due to a bug in rust doc 1.71.0 #113896

--- src/lib.rs
+++ src/lib.rs
@@ -191,7 +191,7 @@ pub use service_info::{
 /// A handler to receive messages from [ServiceDaemon]. Re-export from `flume` crate.
 pub use flume::Receiver;
 
-/// Errors returned by the receiving methods of [Receiver]. Re-export from `flume` crate.
+/// Errors returned by the receiving methods of `Receiver`. Re-export from `flume` crate.
 pub use flume::{RecvError, RecvTimeoutError, TryRecvError};
 
 use std::time::SystemTime;
