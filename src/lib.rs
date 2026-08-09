55266269c35f0b16c6ca8993a7f592b75ff3535e
Han Xu <keepsimple@gmail.com>
2026-08-08 21:34:44 -0700

    fix cargo doc

--- src/lib.rs
+++ src/lib.rs
@@ -188,9 +188,11 @@ pub use service_info::{
     AsIpAddrs, IntoTxtProperties, ResolvedService, ServiceInfo, TxtProperties, TxtProperty,
 };
 
-/// A handler to receive messages from [ServiceDaemon], and the errors its receiving
-/// methods return. Re-export from `flume` crate.
-pub use flume::{Receiver, RecvError, RecvTimeoutError, TryRecvError};
+/// A handler to receive messages from [ServiceDaemon]. Re-export from `flume` crate.
+pub use flume::Receiver;
+
+/// Errors returned by the receiving methods of [Receiver]. Re-export from `flume` crate.
+pub use flume::{RecvError, RecvTimeoutError, TryRecvError};
 
 use std::time::SystemTime;
 
